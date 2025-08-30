14ae9da4057d3af7db140f96c0c990ce494caa67
857l <nik.gusev.2017@bk.ru>
2025-08-30 19:53:29 +0300

    task 013 solved

---
+++ app/src/main/java/com/github/johnnysc/practicetdd/DelayResponse.kt
@@ -0,0 +1,25 @@
+package com.github.johnnysc.practicetdd
+
+import kotlinx.coroutines.delay
+
+interface DelayResponse {
+
+    suspend fun <T> delayAfter(delayInMillis: Long, block: suspend () -> T) : T
+
+    class Base(
+        private val now: Now
+    ) : DelayResponse {
+
+        override suspend fun <T> delayAfter(delayInMillis: Long, block: suspend () -> T): T {
+            val start = now.time()
+            val result = block.invoke()
+            if (delayInMillis > 0){
+                val durationOfBlock = now.time() - start
+                if (durationOfBlock < delayInMillis){
+                    delay(delayInMillis - durationOfBlock)
+                }
+            }
+            return result
+        }
+    }
+}
