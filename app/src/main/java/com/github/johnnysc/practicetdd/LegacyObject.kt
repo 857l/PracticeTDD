b91cc015863968d6781694c62d9b3a90e8cc85cf
857l <nik.gusev.2017@bk.ru>
2025-09-01 20:34:54 +0300

    task 023 solved

---
+++ app/src/main/java/com/github/johnnysc/practicetdd/LegacyObject.kt
@@ -0,0 +1,11 @@
+package com.github.johnnysc.practicetdd
+
+data class LegacyObject(
+    private val text: String,
+    private val lambda: () -> Unit
+) {
+
+    fun go() {
+        lambda.invoke()
+    }
+}
